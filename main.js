a89492b997bbcaf1e13918466a8706ab0a61bdbc
WendyLin650727 <wendy.lin@auo.com>
2026-06-16 15:16:08 +0800

    modify

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ import { spinner } from "./utils/spinner.js";
 try {
   while (true) {
     const query = (
-      await input({ message: "請輸入要搜尋的水果(c、鳳梨、釋迦、蓮霧、芭樂，以上5種水果擇一)：" })
+      await input({ message: "請輸入要搜尋的水果(芒果、鳳梨、釋迦、蓮霧、芭樂，以上5種水果擇一)：" })
     ).trim();
     
     if (query === "") continue;
